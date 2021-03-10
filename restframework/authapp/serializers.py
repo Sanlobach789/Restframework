7b9be02182afa6ef0459465611384b5adfeaa274
sanlobach <sanlobach789@gmail.com>
2021-03-10 13:09:38 +0300

    lesson_2, added react-app

--- restframework/authapp/serializers.py
+++ restframework/authapp/serializers.py
@@ -6,4 +6,4 @@ from authapp.models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'age', 'avatar')
+        fields = ('uuid', 'first_name', 'last_name', 'email', 'age', 'avatar')
---
+++ restframework/frontend/src/components/User.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>
+                    First name
+                </th>
+
+                <th>
+                    Last name
+                </th>
+
+                <th>
+                    Email
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            {users.map((user) => <UserItem user={user} key={user.uuid}/>)}
+            </tbody>
+        </table>
+    )
+}
+
+export default UserList
